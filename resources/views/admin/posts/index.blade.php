dddafae21c77f306a6deb423c67eafee2f21d717
Difelice-e <difelice.emanuele@gmail.com>
2022-05-04 16:38:34 +0200

    aggiornata index.blade.php

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -13,9 +13,9 @@
             <th scope="col">#</th>
             <th scope="col">Titolo</th>
             <th scope="col">Slug</th>
+            <th scope="col">Categoria</th>
             <th scope="col">Data pubblicazione</th>
             <th scope="col">Data creazione</th>
-            <th scope="col"></th>
           </tr>
         </thead>
         <tbody>
@@ -24,7 +24,8 @@
             <td>{{$post->id}}</td>
             <td>{{$post->title}}</td>
             <td>{{$post->slug}}</td>
-            <td>{{$post->published_at}}</td>
+            <td>{{$post->category ? $post->category->name : '-'}}</td>
+            <td>{{$post->published_at ? $post->published_at : '-'}}</td>
             <td>{{$post->created_at}}</td>
             <td>
                 <a class="btn btn-small btn-warning" href="{{route('admin.posts.edit', $post)}}">Edit</a>
